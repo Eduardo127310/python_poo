be31aa53ec8295da8004972cf796758f76488e5b
Eduardo127310 <estudo454@gmail.com>
2026-06-12 16:24:43 -0300

    Resolve exercício 21
    Implementação da solução da questão 21 do curso de Programação Orientada a Objetos do Curso em Vídeo.

---
+++ desafios/desafio21/desafio21.py
@@ -0,0 +1,33 @@
+from rich import print
+class Pen:
+    colors={"vermelho":"red","azul":"blue","verde":"green"}
+    def __init__(self,color):
+        self.color=color.lower()
+        self.true=False
+    
+    def uncork(self):
+        self.true=True
+    
+    def to_write(self,phrase):
+        if self.color in Pen.colors:
+            if self.true == True:
+                print(f"[{self.colors[self.color]}]{phrase}[/]",end="")
+            else:
+                print(f":no_entry_sign: A caneta está tampada!")
+           
+    def break_line(self,number):
+        print(number * "\n")
+
+    
+
+p1=Pen("azul")
+p2=Pen("vermelho")
+p3=Pen("verde")
+p1.uncork()
+p3.uncork()
+p2.uncork()
+p3.uncork()
+p1.to_write("Olá, tudo bem? ")
+p1.break_line(2)
+p2.to_write("Olá, Gafanhoto! ")
+p3.to_write("vamos exercitar! ")
