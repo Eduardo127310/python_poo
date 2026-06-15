a6c32f0359d67750661e92aa8e9b0faf111f8b37
Eduardo127310 <estudo454@gmail.com>
2026-06-15 00:33:42 -0300

    Update desafio22.py

--- desafios/desafio22/desafio22.py
+++ desafios/desafio22/desafio22.py
@@ -18,28 +18,37 @@ class RemotControl:
            3:f"[on dark_green]{3*" "}[/][on gray30]{2*" "}[/]",
            4:f"[on dark_green]{4*" "}[/][on gray30]{1*" "}[/]",
            5:f"[on dark_green]{5*" "}[/]"}
-        formatting2={1:f"[on gold3] 1 [/] 2 3 4 5",
-             2:f"1 [on gold3] 2 [/] 3 4 5",
-             3:f"1 2 [on gold3] 3 [/] 4 5",
-             4:f"1 2 3 [on gold3] 4 [/] 5",
-             5:f"1 2 3 4 [on gold3] 5 [/]"}
+        formatting2 = {
+    1: f"[yellow on yellow] 1 [/]  2   3   4   5 ",
+    2: f" 1  [yellow on yellow] 2 [/]  3   4   5 ",
+    3: f" 1   2  [yellow on yellow] 3 [/]  4   5 ",
+    4: f" 1   2   3  [yellow on yellow] 4 [/]  5 ",
+    5: f" 1   2   3   4  [yellow on yellow] 5 [/]"
+}
         
         if self.on_of == False:
-            box=Panel(":no_entry_sign: [red]A TV está desligada[/]",title="[ TV ]",width=30)
+            box=Panel(":prohibited: [red]A TV está desligada[/]",title="[ TV ]",width=30)
             print(box)
         if self.on_of == True:
             box=Panel(f"CANAL  = {formatting2[self.channel]}\nVOLUME = {formatting[self.volume]}",title="[ TV ]",width=35)
             print(box)
     def changeChannel(self):
-        if self.command == ">" and self.channel < 5:
-            self.channel=self.channel+1
-        else:
-            self.channel = 1
+      if self.channel < 5 and self.command == ">":
+          self.channel=self.channel+1
+      elif self.channel == 5 and self.command == ">":
+          self.channel=1
+      elif  self.channel > 1 and self.command == "<":
+          self.channel=self.channel-1
+      elif  self.channel == 1 and self.command == "<":
+          self.channel=5
+    
     def changeVolume (self):
         if self.command == "+" and self.volume < 5:
             self.volume = self.volume + 1
-        if self.command == "-" and self.volume > 1:
+        elif self.command == "-" and self.volume > 1:
             self.volume=self.volume-1
+        
+        
     def turnOnOFF(self):
         if self.on_of == False and self.command == "@":
             self.on_of=True
